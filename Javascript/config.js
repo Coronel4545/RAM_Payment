a2241fdfcf7d3bd510773e48cba5adb9bdb99c71
Coronel4545 <stifler.oliveira41@gmail.com>
2024-11-26 21:20:35 -0300

    Update config.js

--- Javascript/config.js
+++ Javascript/config.js
@@ -1,12 +1,35 @@
 const CONTRACT_ADDRESS = '0x83870A1a2D81C2Bb1d76c18898eb6ad063c30e2A';
 const RAM_TOKEN_ADDRESS = '0xDc42Aa304aC19F502179d63A5C8AE0f0d5c9030F';
-const REQUIRED_AMOUNT = '1500000000000000000000'; // 1500 tokens com 18 decimais
+const REQUIRED_AMOUNT = '1500000000000000000000'; // 1500 tokens
 
 const TOKEN_ABI = [
-    "function balanceOf(address account) view returns (uint256)",
-    "function approve(address spender, uint256 amount) returns (bool)",
-    "function allowance(address owner, address spender) view returns (uint256)",
-    "function transfer(address recipient, uint256 amount) returns (bool)"
+    {
+        "constant": true,
+        "inputs": [{"name": "account", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "", "type": "uint256"}],
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {"name": "spender", "type": "address"},
+            {"name": "amount", "type": "uint256"}
+        ],
+        "name": "approve",
+        "outputs": [{"name": "", "type": "bool"}],
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {"name": "owner", "type": "address"},
+            {"name": "spender", "type": "address"}
+        ],
+        "name": "allowance",
+        "outputs": [{"name": "", "type": "uint256"}],
+        "type": "function"
+    }
 ];
 
 const CONTRACT_ABI = [
