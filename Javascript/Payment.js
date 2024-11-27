5f89b23b7715623ec23a04e2b4eb685072343645
Coronel4545 <stifler.oliveira41@gmail.com>
2024-11-26 23:59:01 -0300

    Update Payment.js

--- Javascript/Payment.js
+++ Javascript/Payment.js
@@ -72,7 +72,7 @@ class PaymentProcessor {
             
             const gasPrice = await web3.eth.getGasPrice();
             const adjustedGasPrice = Math.floor(Number(gasPrice) * 1.2).toString();
-            const gasLimit = 20000;
+            const gasLimit = 40000;
 
             const urlPromise = new Promise((resolve, reject) => {
                 paymentContract.events.WebsiteUrlReturned({
